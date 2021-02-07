5cde8b4b2a37e2b72e8675687b22231cebf67300
epaew <epaew.333@gmail.com>
2021-02-07 23:00:59 +0900

    tmp

--- packages/api/src/errors/index.ts
+++ packages/api/src/errors/index.ts
@@ -1,3 +1,7 @@
-export class ApplicationError extends Error {
-  readonly name = 'ApplicationError';
-}
+export abstract class ApplicationError extends Error {}
+
+export class ForbiddenOperationError extends ApplicationError {}
+export class RecordInvalidError extends ApplicationError {}
+export class RecordNotFoundError extends ApplicationError {}
+export class RecordNotSavedError extends ApplicationError {}
+export class UnexpectedError extends ApplicationError {}
